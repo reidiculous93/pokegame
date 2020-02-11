56190b73545364225ce1fa1b5c315300635a7716
Reid Vrana <reidiculous@Reids-MBP.lan>
2020-02-10 22:18:09 -0600

    remove pokemon

--- src/pokegame.js
+++ src/pokegame.js
@@ -11,22 +11,13 @@ class Pokegame extends Component {
             {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
             {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
             {id: 94, name: 'Gengar', type: 'ghost', base_experience: 225},
-            {id: 133, name: 'Eevee', type: 'normal', base_experience: 65},
-            {id: 28, name: 'Sandslash', type: 'ground', base_experience: 95},
-            {id: 34, name: 'Nidoking', type: 'poison', base_experience: 190},
-            {id: 38, name: 'Ninetails', type: 'fire', base_experience: 145},
-            {id: 45, name: 'Vileplume', type: 'grass', base_experience: 180},
-            {id: 17, name: 'Pidgeotto', type: 'flying', base_experience: 40},
-            {id: 87, name: 'Dewgong', type: 'water', base_experience: 170},
-            {id: 96, name: 'Drowzee', type: 'psychic', base_experience: 65},
-            {id: 98, name: 'Krabby', type: 'water', base_experience: 75},
-            {id: 104, name: 'Cubone', type: 'ground', base_experience: 100},
+            {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
           ]
     };
     render() {
         let hand1 = [];
         let hand2 = [...this.props.pokemon];
-        while(hand1.length < hand2.length){
+        while(hand1.length < 4){
             let randIdx = Math.floor(Math.random() * hand2.length);
             let randPokemon = hand2.splice(randIdx, 1)[0];
             hand1.push(randPokemon);
